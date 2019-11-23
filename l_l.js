257eb7bcb2e06a760d79849fccba658d417c9b46
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-11-22 23:10:10 -0800

    Update l_l.js

--- l_l.js
+++ l_l.js
@@ -2,4 +2,5 @@ var addTwoNumbers = function(l1, l2) {
      let l1_hashmap = new Map
      let l1_digit_counter = 0
      for(let ptr = l1; ptr != null; ptr = ptr.next){
-`        l1_hashmap.set(l1_digit_counter, ptr.val)
+        l1_hashmap.set(l1_digit_counter, ptr.val)
+        l1_digit_counter ++ 
