f27d0f248479a7ca38063ca4c7d45780447844be
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-11-25 23:26:09 -0800

    Update l_l.js

--- l_l.js
+++ l_l.js
@@ -33,4 +33,5 @@ var addTwoNumbers = function(l1, l2) {
      for(let i = l2_hashmap.size-1; i >= 0; i--){
          let num = l2_hashmap.get(i);
          let str = num.toString();
+         l2_flipped_string = l2_flipped_string + str;
      
