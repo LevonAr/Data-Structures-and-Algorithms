7eda901ed65e7e89d058dbd967595251b759b893
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-11-30 23:37:25 -0800

    Update l_l.js

--- l_l.js
+++ l_l.js
@@ -46,6 +46,9 @@ var addTwoNumbers = function(l1, l2) {
      
      let ptr
      
+     class Linked_List{
+
+     
      for(let i=0; i < 3; i++){
           if(i==0){
                ptr = new ListNode()
