f401a25d42896ed1b3be5b811083a991df63cc1d
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-03 13:11:36 -0800

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -22,4 +22,11 @@ class Linked_List {
         }
     }
 }
-    
+
+var addTwoNumbers = function(l1, l2) {
+     let l1_hashmap = new Map
+     let l1_digit_counter = 0
+     for(let ptr = l1; ptr != null; ptr = ptr.next){
+        l1_hashmap.set(l1_digit_counter, ptr.val)
+        l1_digit_counter ++ 
+     }    
