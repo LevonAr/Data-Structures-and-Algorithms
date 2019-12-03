338c21cf0df6018b24336625d1ff02abde727ea0
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-03 13:14:24 -0800

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -3,7 +3,6 @@ class Linked_List {
         this.head = null;
         this.size = 0;
     }
-
     add(element){
         if(this.head==null){
             this.head = new ListNode(element)
@@ -23,15 +22,14 @@ class Linked_List {
     }
 }
 
-
 var addTwoNumbers = function(l1, l2) {
      let l1_hashmap = new Map
      let l1_digit_counter = 0
      for(let ptr = l1; ptr != null; ptr = ptr.next){
         l1_hashmap.set(l1_digit_counter, ptr.val)
         l1_digit_counter ++ 
-     }    
-
+     }
+    
         
      let l2_hashmap = new Map
      let l2_digit_counter = 0
@@ -46,8 +44,8 @@ var addTwoNumbers = function(l1, l2) {
          let num = l1_hashmap.get(i)
          let str = num.toString()
          l1_flipped_string = l1_flipped_string + str
-     }    
-
+     }
+     
      let l1_flipped_number = parseInt(l1_flipped_string)
           
      let l2_flipped_string = ""
@@ -57,6 +55,7 @@ var addTwoNumbers = function(l1, l2) {
          let str = num.toString()
          l2_flipped_string = l2_flipped_string + str
      }
+    
      let l2_flipped_number = parseInt(l2_flipped_string)
      
      let final_sum = l1_flipped_number + l2_flipped_number
@@ -65,13 +64,13 @@ var addTwoNumbers = function(l1, l2) {
 
      let final_sum_digits = final_sum_string.length
                
-     let final_linked_list = new Linked_List     
+     let final_linked_list = new Linked_List
      
      for(let i = final_sum_digits-1; i >= 0; i--){
          let char = final_sum_string.charAt(i)
          let num = parseInt(char)         
          final_linked_list.add(num)   
      }
-        return (final_linked_list.head)    
-};
     
+    return (final_linked_list.head)    
+};
