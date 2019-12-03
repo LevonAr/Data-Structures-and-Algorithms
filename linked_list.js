9ca06d6ebd478f516d33c5fa6e0a1ded52707a96
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-03 13:11:18 -0800

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -3,3 +3,23 @@ class Linked_List {
         this.head = null;
         this.size = 0;
     }
+
+    add(element){
+        if(this.head==null){
+            this.head = new ListNode(element)
+            console.log(this.head, this.size)
+            this.size ++
+        }
+        else{
+            let Ptr = this.head
+            while (Ptr.next) { 
+                Ptr = Ptr.next; 
+            } 
+            console.log(Ptr, this.size)                        
+            Ptr.next = new ListNode(element) 
+            console.log(Ptr, this.size)            
+            this.size ++
+        }
+    }
+}
+    
