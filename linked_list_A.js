0733f66352bb68e7c9f08216761d174bbbb37e3e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-10 23:48:10 -0800

    Update linked_list_A.js

--- linked_list_A.js
+++ linked_list_A.js
@@ -4,3 +4,7 @@ var addTwoNumbers = function(l1, l2) {
     if (l1 || l2) {
         const val1 = l1 ? l1.val : 0
         const val2 = l2 ? l2.val : 0
+        const next1 = l1 ? l1.next : null
+        const next2 = l2 ? l2.next : null
+        const val = carry ? val1 + val2 + 1 : val1 + val2
+        node = new ListNode(val % 10)
