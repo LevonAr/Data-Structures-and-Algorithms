ddac98f3c15f2f23c67d544a1c2fa4e19e614958
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-10 23:49:37 -0800

    Update linked_list_A.js

--- linked_list_A.js
+++ linked_list_A.js
@@ -11,3 +11,4 @@ var addTwoNumbers = function(l1, l2) {
         node.next = addTwoNumbers(next1, next2, val >= 10)  
     } else if (carry) {
         node = new ListNode(1)
+        node.next = null
