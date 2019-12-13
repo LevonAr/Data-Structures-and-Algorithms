bb156d2135ef0f1d505272504840b471afe15a5b
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-12 23:50:53 -0800

    Update weakmap.js

--- weakmap.js
+++ weakmap.js
@@ -2,7 +2,9 @@
    const key1 = {}; 
    weakmap1.set(key1, 42); 
    document.write(weakmap1.get(key1)); 
-   
+
+   const weakmap2 = new WeakMap ()
+
    const key2 = {};
 
    const weakmap2 = new WeakMap ()
