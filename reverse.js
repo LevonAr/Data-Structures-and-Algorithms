6f721be537d1d2811af4446b2516085c88fc8282
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-20 23:59:34 -0800

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -2,3 +2,8 @@ var promise = new Promise(function(resolve, reject) {
   const x = "geeksforgeeks"; 
   const y = "geeksforgeeks"
   if(x === y) { 
+  resolve(); 
+  } else { 
+    reject(); 
+  } 
+}); 
