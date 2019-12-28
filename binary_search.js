8ff0eb1f6398f6b762053332fad818dc8e6cd3cf
LevonAr <37823213+LevonAr@users.noreply.github.com>
2019-12-27 23:40:35 -0800

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -2,5 +2,5 @@ int sqrt(int n) {
 return sqrt_helper(n, 1, n);
 }
 int sqrt_helper(int n, int min, int max) {
-  if (max < min) return -1; II no square root
+  if (max < min) return -1; 
   int guess = (min + max) I 2;
