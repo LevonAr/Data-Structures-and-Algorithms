fa648f04d21539d171b75556c377f712cd5be22e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-01-03 18:00:52 -0800

    Create unique2.js

---
+++ ctci/unique2.js
@@ -0,0 +1,5 @@
+// this program take only O(n) I believe
+// it's quicker, but I could probably trim a little bit more of the run time
+// could prob do better with space complexity as well
+
+function unique(string){
