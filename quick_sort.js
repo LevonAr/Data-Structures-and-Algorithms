676c55e576a66320e5a72ab5e8e09451bdf684bd
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-01-11 23:10:08 -0800

    Update quick_sort.js

--- quick_sort.js
+++ quick_sort.js
@@ -17,3 +17,7 @@ function partition(arr, pivot, left, right){
    var pivotValue = arr[pivot],
        partitionIndex = left;
    for(var i = left; i < right; i++){
+      if(arr[i] < pivotValue){
+         swap(arr, i, partitionIndex);
+         partitionIndex++;
+      }
