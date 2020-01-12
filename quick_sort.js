6b273af5b8d15396adf0d6a9156ac7daf79e069b
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-01-11 23:07:07 -0800

    Update quick_sort.js

--- quick_sort.js
+++ quick_sort.js
@@ -7,3 +7,5 @@ function quickSort(arr, left, right){
       pivot = right;
       partitionIndex = partition(arr, pivot, left, right);
       
+      quickSort(arr, left, partitionIndex - 1);
+      quickSort(arr, partitionIndex + 1, right);
