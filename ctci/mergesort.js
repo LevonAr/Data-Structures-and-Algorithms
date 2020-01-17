ac1fbf955f699aa4daf95908123705b32b1f33f5
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-01-16 23:42:42 -0800

    Update mergesort.js

--- ctci/mergesort.js
+++ ctci/mergesort.js
@@ -10,3 +10,11 @@ function mergeSort(array,half = array.length/2){
   function merger(left,right){
     
     const arr = [];
+      
+    while(left.length && right.length){
+      if(left[0] < right [0]){
+        arr.push(left.shift())
+      }else{
+        arr.push(right.shift())
+      }
+    }
