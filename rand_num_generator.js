e2de2b933e1b53aee10a7f2d8d06763e3b3480af
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-01-25 23:43:32 -0800

    Update rand_num_generator.js

--- rand_num_generator.js
+++ rand_num_generator.js
@@ -4,3 +4,6 @@ module.exports =
         let random_array =[]
             for(i=0;i<50;i++){
                 random_array.push(Math.floor(Math.random()*1000000))
+            }
+        return random_array
+    }
