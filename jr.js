b6032ab711103996b64869a9ac16cf4bdfdeeb63
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-02-07 23:46:43 -0800

    Update jr.js

--- jr.js
+++ jr.js
@@ -3,4 +3,4 @@ module.exports =
     random: function(){
         let random_array =[]
             for(i=0;i<10000000;i++){
-                random_array.push(Math.floor
+                random_array.push(Math.floor(Math.random()*1000000))
