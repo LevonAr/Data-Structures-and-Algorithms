dd195bd24bad7d69f1a6d77bf771d50381d0bc1e
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-02-26 23:46:25 -0800

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,9 +1,7 @@
 var Students = [ 
     { rollNo: 21, name: 'Alpha' }, 
     { rollNo: 22, name: 'Beta' }, 
-    { rollNo: 23, name: 'Gamma' }, 
-    { rollNo: 24, name: 'Delta' }, 
-    { rollNo: 25, name: 'Omega'} 
+    { rollNo: 23, name: 'Gamma' },
 ]; 
 var StudentRollNo = []; 
 Students.forEach(function (Student) { 
